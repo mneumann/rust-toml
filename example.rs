7ce8ce951e924850f57f9515964ae2e16723c10a
Michael Neumann <mneumann@ntecs.de>
2014-01-05 19:45:44 +0100

    Use BufferedReader

--- example.rs
+++ example.rs
@@ -1,11 +1,16 @@
 use std::io::mem::MemReader;
+use std::io::buffered::BufferedReader;
 use std::io::File;
 
 mod toml;
 
 fn main() {
-  let contents = File::open(&Path::new(std::os::args()[1])).read_to_end();
-  let mut rd = MemReader::new(contents);
+  let path = Path::new(std::os::args()[1]);
+  let mut file = File::open(&path);
+
+  //let contents = file.read_to_end();
+  //let mut rd = MemReader::new(contents);
+  let mut rd = BufferedReader::new(file);
 
   let mut builder = toml::ValueBuilder::new();
   let mut parser = toml::Parser::new(&mut rd);
