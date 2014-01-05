96d9271a038b453a9bef33f2dc0c3e406af1738d
Michael Neumann <mneumann@ntecs.de>
2014-01-05 15:32:00 +0100

    Greatly simplify parse() by using look-ahead

--- toml.rs
+++ toml.rs
@@ -117,7 +117,7 @@ fn skip_comment(rd: &mut MemReader) {
 
 trait Visitor {
     fn section(&mut self, name: ~str, is_array: bool) -> bool;
-    fn pair(&mut self, key: &str, val: Value) -> bool;
+    fn pair(&mut self, key: ~str, val: Value) -> bool;
 }
 
 struct TOMLVisitor {
@@ -142,12 +142,12 @@ impl Visitor for TOMLVisitor {
         self.current_path = name;
         return true
     }
-    fn pair(&mut self, key: &str, val: Value) -> bool {
+    fn pair(&mut self, key: ~str, val: Value) -> bool {
         debug!("Pair: {} {:s}", key, val.to_str());
         let mut m = self.root.find_or_insert(self.current_path.clone(), Map(HashMap::new())); // XXX: remove clone
         match *m {
             Map(ref mut map) => {
-                let ok = map.insert(key.to_owned(), val);
+                let ok = map.insert(key, val);
                 return ok
             }
             _ => { return false }
@@ -155,132 +155,119 @@ impl Visitor for TOMLVisitor {
     }
 }
 
-fn parse_section_identifier(rd: &mut MemReader, current_char: Option<char>) -> (~str, Option<char>) {
+fn read_token(rd: &mut MemReader, current_char: Option<char>, f: |char| -> bool) -> (~str, Option<char>) {
     let mut current_char = current_char;
-    let mut section_name = ~"";
+    let mut token = ~"";
     loop {
         match current_char {
             Some(ch) => {
-                match ch { 
-                    'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' | '.' | '_'=> {
-                        section_name.push_char(ch);
-                    }
-                    _ => { break }
-                }
+                if f(ch) { token.push_char(ch) }
+                else { break }
             }
             None => { break }
         }
         current_char = read_char_opt(rd); // advance
     }
 
-    return (section_name, current_char);
+    return (token, current_char);
+}
+
+fn parse_section_identifier(rd: &mut MemReader, current_char: Option<char>) -> (~str, Option<char>) {
+    read_token(rd, current_char, |ch| {
+        match ch {
+            'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' | '.' | '_' => true,
+            _ => false
+        }
+    })
+}
+
+fn skip_whitespaces(rd: &mut MemReader, current_char: Option<char>) -> Option<char> {
+    let mut current_char = current_char;
+    loop {
+        match current_char {
+            Some(' ') | Some('\t') | Some('\r') | Some('\n') => {
+                current_char = read_char_opt(rd); // advance
+            }
+            _ => { break }
+        }
+    }
+    return current_char;
 }
 
 fn parse<V: Visitor>(rd: &mut MemReader, visitor: &mut V) -> bool {
-    enum State {
-        st_toplevel,
-        st_ident,
-        st_assign_wanted,
-        st_value_wanted
-    }
-
-    let mut state = st_toplevel;
-    let mut buf: ~str = ~"";
-
     let mut current_char: Option<char> = read_char_opt(rd);
 
     loop {
-        match state {
-            st_toplevel => {
-                if current_char.is_none() { return true }
-                let ch = current_char.unwrap();
-                match ch {
-                    // ignore whitespace
-                    '\r' | '\n' | ' ' | '\t' => { }
+        if current_char.is_none() { return true }
+        let ch = current_char.unwrap();
+        match ch {
+            // ignore whitespace
+            '\r' | '\n' | ' ' | '\t' => { }
 
-                    // comment
-                    '#' => {
-                        // skip to end of line
-                        loop {
-                            current_char = read_char_opt(rd);
-                            match current_char {
-                                Some('\n') => { break }
-                                None => { return true }
-                                _ => { /* skip */ }
-                            }
-                        }
+            // comment
+            '#' => {
+                // skip to end of line
+                loop {
+                    current_char = read_char_opt(rd);
+                    match current_char {
+                        Some('\n') => { break }
+                        None => { return true }
+                        _ => { /* skip */ }
                     }
+                }
+            }
 
-                    // section
-                    '[' => {
+            // section
+            '[' => {
+                current_char = read_char_opt(rd); // advance
+                let mut double_section = false;
+                match current_char {
+                    Some('[') => {
+                        double_section = true;
                         current_char = read_char_opt(rd); // advance
-                        let mut double_section = false;
-                        match current_char {
-                            Some('[') => {
-                                double_section = true;
-                                current_char = read_char_opt(rd); // advance
-                            }
-                            _ => {}
-                        }
-
-                        let (section_name, ch) = parse_section_identifier(rd, current_char);
-                        current_char = ch;
-
-                        match current_char {
-                            Some(']') => { /* ok */ }
-                            _ => { return false }
-                        }
-
-                        if double_section {
-                            current_char = read_char_opt(rd); // advance
-                            if current_char != Some(']') { return false }
-                        }
-
-                        visitor.section(section_name, double_section);
                     }
+                    _ => {}
+                }
 
-                    // identifier
-                    'a' .. 'z' | 'A' .. 'Z' | '_' => {
-                        state = st_ident;
-                        buf.push_char(ch)
-                    }
+                let (section_name, ch) = parse_section_identifier(rd, current_char);
+                current_char = ch;
 
+                match current_char {
+                    Some(']') => { /* ok */ }
                     _ => { return false }
                 }
-                current_char = read_char_opt(rd); // advance
+
+                if double_section {
+                    current_char = read_char_opt(rd); // advance
+                    if current_char != Some(']') { return false }
+                }
+
+                visitor.section(section_name, double_section);
             }
-            st_ident => {
-                if current_char.is_none() { return false }
-                let ch = current_char.unwrap();
-                match ch {
-                    'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' | '_'=> {
-                        buf.push_char(ch);
+
+            // identifier
+            'a' .. 'z' | 'A' .. 'Z' | '_' => {
+
+                let (ident, ch) = read_token(rd, current_char, |ch| {
+                    match ch {
+                        'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' | '_' => true,
+                        _ => false
                     }
+                });
 
-                    '\r' | '\n' | ' ' | '\t' => { state = st_assign_wanted }
+                current_char = ch;
+                current_char = skip_whitespaces(rd, current_char);
 
-                    '=' => { state = st_value_wanted }
+                // assign wanted
+                if current_char != Some('=') { return false }
+                
+                // XXX: current_char = read_char_opt(rd); // advance
+                visitor.pair(ident, parse_value(rd)); // XXX: parse_value is not using peeking yet
+            }
 
-                    _ => { return false }
-                }
-                current_char = read_char_opt(rd); // advance
-            }
-            st_assign_wanted => {
-                if current_char.is_none() { return false }
-                match current_char.unwrap() {
-                    '\r' | '\n' | ' ' | '\t' => { } 
-                    '=' => { state = st_value_wanted }
-                    _ => { return false }
-                }
-                current_char = read_char_opt(rd); // advance
-            }
-            st_value_wanted => {
-                visitor.pair(buf, parse_value(rd));
-                buf.truncate(0);
-                state = st_toplevel;
-                current_char = read_char_opt(rd); // advance
-            }
-        }
+            _ => { return false }
+        } /* end match */
+        current_char = read_char_opt(rd); // advance
     }
 
     assert!(false);
